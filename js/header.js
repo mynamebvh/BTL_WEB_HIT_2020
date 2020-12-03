428dc5b12c2799c9854b4017980264aeab742ba2
Hoang Bui <mynamebvh@gmail.com>
2020-12-04 04:54:34 +0700

    commit hoan thien 04/12

--- js/header.js
+++ js/header.js
@@ -67,3 +67,85 @@ setInterval(() => {
   slides[random].style.border = "2px solid white";
   random++;
 },3000)
+
+
+const app = document.getElementById("app");
+
+var historys = [];
+
+function save(arr, value) {
+  arr.push(value);
+
+  return arr;
+}
+
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  var value = save(historys,scroll).slice(-2);
+  var length = value.length;
+  // localStorage.setItem("history", value);
+
+  var header = document.getElementsByTagName("header");
+  var headerContainer = document.getElementById("header");
+  var search = document.getElementById("search");
+  var headerLi = document.querySelectorAll(".header__bottom--menu > li");
+  var headerBottom = document.querySelector(".header__bottom");
+  var headerTop = document.querySelector(".header__top");
+  var navItem = document.querySelector(".nav__item ");
+  var headerBottomMenu = document.querySelector(".header__bottom--menu");
+
+  if(value[1]  > value[0]){
+    search.classList.add("hidden");
+    headerBottom.classList.remove("drop__header");
+    // headerContainer.classList.add("shadow");
+    headerBottom.classList.remove("col-xl-12");
+    headerBottom.classList.add("col-xl-7");
+
+    navItem.classList.remove("col-xl-4");
+    navItem.classList.add("col-xl-3");
+    headerLi[0].classList.add("hidden");
+    headerLi[5].classList.add("hidden");
+
+    headerLi[6].classList.add("hidden");
+    headerLi[7].classList.add("hidden");
+    headerLi[9].classList.add("hidden");
+    headerTop.style.height = "50px";
+
+    header[0].classList.add("header__drop");
+    // headerContainer.classList.add("container-fluid")
+    headerBottomMenu.classList.add("header__bottom--menu-drop");
+  }
+  else {
+    search.classList.remove("hidden");
+    headerBottom.classList.add("drop__header");
+    headerBottom.classList.add("col-xl-12");
+    headerBottom.classList.remove("col-xl-7");
+
+    navItem.classList.add("col-xl-4");
+    navItem.classList.remove("col-xl-3");
+    headerLi[0].classList.remove("hidden");
+    headerLi[5].classList.remove("hidden");
+
+    headerLi[6].classList.remove("hidden");
+    headerLi[7].classList.remove("hidden");
+    headerLi[9].classList.remove("hidden");
+    headerTop.style.height = "124px";
+
+    header[0].classList.remove("header__drop");
+    headerBottomMenu.classList.remove("header__bottom--menu-drop");
+    // headerContainer.classList.remove("shadow");
+    console.log("len");
+  }
+  
+
+
+
+  // var temp = scroll;
+  // if(temp > scroll)
+  // console.log("lên");
+  // else 
+  // console.log("xuống");
+});
+
+
